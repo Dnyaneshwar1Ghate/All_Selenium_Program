c5c020f4fa7a4e882348b66baee03cbe2a0749f5
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2025-11-12 00:02:46 +0530

    first

---
+++ src/main/java/Seleninum/First.java
@@ -0,0 +1,15 @@
+package Seleninum;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class First {
+	
+	public static void main(String[] args) {
+
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.demoqa.com");
+		
+	}
+
+}
